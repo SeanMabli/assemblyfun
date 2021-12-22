9a2573157024a1bc97432aca4eb61e33fae62cdc
sean mabli <70709738+seanmabli@users.noreply.github.com>
2021-12-22 17:39:17 +0000

    work on array and int

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # assembly fun
 addition - works with 1-digit + 1-digit that don't sum to 2-digit<br />
-array  - doesn't work <br />
+array  - adds array of 1-digit that don't sum to 2-digit <br />
 double - doesn't work <br />
 float - doesn't work <br />
 helloworld - working <br />
-int - working <br />
+int - working with 1-digit positive integers <br />
---
+++ array/array.asm
@@ -0,0 +1,30 @@
+section	.text
+  global _start
+	
+_start:	
+  mov  eax, len
+  mov  ebx, 0
+  mov  ecx, array
+
+  loop:
+    add  ebx, [ecx]
+    add  ecx, 1
+    dec  eax
+    jnz  loop
+
+  add ebx, '0'
+  mov [sum], ebx
+
+  mov  edx, 1
+  mov  ecx, sum
+  mov  ebx, 1
+  mov  eax, 4
+  int  0x80
+	
+  mov	eax, 1
+  int	0x80
+
+section	.data
+  array db 2, 1, 2, 3
+  len equ $ - array
+  sum db  0
